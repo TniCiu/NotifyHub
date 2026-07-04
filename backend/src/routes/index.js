fa99a9bb49b584882821d9434d7faf693d915a20
tin.huynhtrong <tin.huynhtrong@fssc.gmail.com>
2026-07-04 16:06:57 +0700

    feat(auth): implement user authentication

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,7 +1,15 @@
 const router = require("express").Router()
-const AuthController = require("../controllers/auth.controller")
+const UserController = require("../controllers/user.controller")
+const authMiddleware = require("../middlewares/auth.middleware")
+const {
+    validateRegister,
+    validateLogin,
+    validateUserId,
+} = require("../middlewares/user.validate.middleware")
 
-router.post("/login", AuthController.login)
-router.post("/register", AuthController.register)
+router.post("/login", validateLogin, UserController.login)
+router.post("/register", validateRegister, UserController.register)
+router.get("/users", authMiddleware, UserController.findAll)
+router.get("/users/:id", authMiddleware, validateUserId, UserController.findById)
 
 module.exports = router
