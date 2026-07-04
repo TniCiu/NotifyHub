90a002e238846259eb20a64484af1b7ef7d2d316
tin.huynhtrong <tin.huynhtrong@fssc.gmail.com>
2026-07-04 16:07:46 +0700

    feat(user) custom message reponse user

--- backend/src/constants/messages/user.message.js
+++ backend/src/constants/messages/user.message.js
@@ -1,6 +1,7 @@
 const USER_MESSAGES = {
   GET_ALL_SUCCESS: "Users retrieved successfully",
   GET_DETAIL_SUCCESS: "User retrieved successfully",
+  ID_INVALID: "User id is invalid",
   NOT_FOUND: "User not found",
 };
 
